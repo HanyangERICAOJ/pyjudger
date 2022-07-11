3f1c2953a34bc9f88d74335e0084a4a526e60e6e
hyoseok <janghs0604@gmail.com>
2022-07-11 11:13:54 +0900

    fix: hide compile stdout and stderr

--- compile.py
+++ compile.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def compile(submission_path: str, compile_command: str) -> bool:
-    compile_process = subprocess.Popen(compile_command, shell=True, cwd=submission_path)
+    compile_process = subprocess.Popen(compile_command, shell=True, cwd=submission_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     compile_process.communicate()
     return compile_process.returncode == 0
